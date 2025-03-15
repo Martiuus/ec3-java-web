122f1ce7a90ac3dd43d739d1f8dc95bfa4706294
aaronp636 <aaronp636@gmail.com>
2025-03-15 13:09:51 -0500

    Ejercicio 1 - Aaron

--- src/main/java/pe/edu/idat/ec3_java_web/controller/comisionVendedoresController.java
+++ src/main/java/pe/edu/idat/ec3_java_web/controller/comisionVendedoresController.java
@@ -2,5 +2,46 @@ package pe.edu.idat.ec3_java_web.controller;
 
 // AARON PINEDA
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.ec3_java_web.model.comisionVendedoresModel;
+
+@Controller
 public class comisionVendedoresController {
+
+    @GetMapping("/comisionVendedores")
+    public String formularioComision(Model model){
+        model.addAttribute("comisionmodel", new comisionVendedoresModel());
+        model.addAttribute("visualizaralerta", false);
+        return "comisionVendedores";
+    }
+
+    @PostMapping("/calcularventas")
+    public String calcularVentas(@ModelAttribute("comisionmodel") comisionVendedoresModel c, Model model){
+        Double ventasTotales = c.getVentasMensuales();
+        Double comision = 0.0;
+
+        if (ventasTotales < 1000){
+            comision = ventasTotales * 0.03;
+            c.setComision(Double.valueOf(String.format("%.2f", comision)));
+        } else if (ventasTotales < 5000) {
+            comision = ventasTotales * 0.05;
+            c.setComision(Double.valueOf(String.format("%.2f", comision)));
+        } else if (ventasTotales < 10000) {
+            comision = ventasTotales * 0.07;
+            c.setComision(Double.valueOf(String.format("%.2f", comision)));
+        }else {
+            comision = ventasTotales * 0.1;
+            c.setComision(Double.valueOf(String.format("%.2f", comision)));
+        }
+
+        model.addAttribute("resultado", String.format("%.2f",comision));
+        model.addAttribute("visualizaralerta", true);
+        return "comisionVendedores";
+
+    }
 }
--- src/main/java/pe/edu/idat/ec3_java_web/model/comisionVendedoresModel.java
+++ src/main/java/pe/edu/idat/ec3_java_web/model/comisionVendedoresModel.java
@@ -3,4 +3,23 @@ package pe.edu.idat.ec3_java_web.model;
 // AARON PINEDA
 
 public class comisionVendedoresModel {
+    private Double ventasMensuales;
+    private Double comision;
+
+    public Double getComision() {
+        return this.comision;
+    }
+
+    public void setComision(Double comision) {
+        this.comision = comision;
+    }
+
+    public Double getVentasMensuales() {
+        return ventasMensuales;
+    }
+
+    public void setVentasMensuales(Double ventasMensuales) {
+        this.ventasMensuales = ventasMensuales;
+    }
+
 }
