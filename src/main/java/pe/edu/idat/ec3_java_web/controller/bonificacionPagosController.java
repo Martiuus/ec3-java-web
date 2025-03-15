00c801a15aa43300f280046ae8a0bacda842ef05
Martiuus <mzf.ycb.11@gmail.com>
2025-03-15 13:16:46 -0500

    Ejercicio bonificacion de pagos resuelto

--- src/main/java/pe/edu/idat/ec3_java_web/controller/bonificacionPagosController.java
+++ src/main/java/pe/edu/idat/ec3_java_web/controller/bonificacionPagosController.java
@@ -2,5 +2,54 @@ package pe.edu.idat.ec3_java_web.controller;
 
 // ALONSO ZAMBRANO
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.ec3_java_web.model.bonificacionPagosModel;
+
+
+@Controller
 public class bonificacionPagosController {
+
+    @GetMapping("/bonificacionPagos")
+    public String formularioBonificacionPagos(Model model){
+        model.addAttribute("pagosmodel", new bonificacionPagosModel());
+        model.addAttribute("visualizaralerta", false);
+        return "bonificacionPagos";
+    }
+
+    @PostMapping("/calcularPagos")
+    public String calcularPagos(@ModelAttribute("pagosmodel") bonificacionPagosModel pg, Model model){
+        Double montoCo = pg.getMontoCompra();
+        Integer diasPa = pg.getDiasPago();
+        Double desctPa;
+        Double total;
+        String diagnostico = "";
+        String estiloDiagnostico = "alert-primary";
+
+        if (diasPa < 7){
+            desctPa = 0.1;
+            total = montoCo * desctPa;
+            diagnostico = "El monto total a pagar es: " + total;
+
+        }else if (diasPa < 15){
+            desctPa = 0.05;
+            total = montoCo * desctPa;
+            diagnostico = "El monto total a pagar es: " + total;
+
+        } else {
+            total = montoCo;
+            diagnostico = "El monto total a pagar es: " + total;
+
+        }
+
+        model.addAttribute("resultado", " " + diagnostico);
+        model.addAttribute("visualizaralerta", true);
+        model.addAttribute("estilodiagnostico", estiloDiagnostico);
+        return "bonificacionPagos";
+
+    }
+
 }
--- src/main/java/pe/edu/idat/ec3_java_web/model/bonificacionPagosModel.java
+++ src/main/java/pe/edu/idat/ec3_java_web/model/bonificacionPagosModel.java
@@ -3,4 +3,40 @@ package pe.edu.idat.ec3_java_web.model;
 // ALONSO ZAMBRANO
 
 public class bonificacionPagosModel {
+    private double montoCompra;
+    private int diasPago;
+    private double descuento;
+    private double totalPagar;
+
+    public double getMontoCompra() {
+        return montoCompra;
+    }
+
+    public void setMontoCompra(double montoCompra) {
+        this.montoCompra = montoCompra;
+    }
+
+    public int getDiasPago() {
+        return diasPago;
+    }
+
+    public void setDiasPago(int diasPago) {
+        this.diasPago = diasPago;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getTotalPagar() {
+        return totalPagar;
+    }
+
+    public void setTotalPagar(double totalPagar) {
+        this.totalPagar = totalPagar;
+    }
 }
