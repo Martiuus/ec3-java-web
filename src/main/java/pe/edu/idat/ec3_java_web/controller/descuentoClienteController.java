9a7a216bfa2ab43c64bdff1c8b9ba8b10fd1b759
aaronp636 <aaronp636@gmail.com>
2025-03-16 13:43:47 -0500

    Parte final Aaron

--- src/main/java/pe/edu/idat/ec3_java_web/controller/descuentoClienteController.java
+++ src/main/java/pe/edu/idat/ec3_java_web/controller/descuentoClienteController.java
@@ -2,5 +2,58 @@ package pe.edu.idat.ec3_java_web.controller;
 
 // AARON PINEDA
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.ec3_java_web.model.descuentoClienteModel;
+
+@Controller
 public class descuentoClienteController {
+
+    @GetMapping("/descuentoCliente")
+    public String formularioDescuentoCliente(Model model){
+        model.addAttribute("descuentomodel", new descuentoClienteModel());
+        model.addAttribute("visualizaralerta", false);
+        return "descuentoCliente";
+    }
+
+    @PostMapping("/calcularDescuento")
+    public String calcularDescuento(@ModelAttribute("descuentomodel") descuentoClienteModel d, Model model){
+
+        Double montoCo = d.getMonto();
+        int aniosAn = d.getAnios();
+        Double descuento = 0.0;
+        Double totalPagar = 0.0;
+
+        if (aniosAn < 1){
+            descuento = montoCo * 0.02;
+            totalPagar = montoCo - descuento;
+
+        } else if (aniosAn < 3) {
+            descuento = montoCo * 0.05;
+            totalPagar = montoCo - descuento;
+            
+        } else if (aniosAn < 5) {
+            descuento = montoCo * 0.08;
+            totalPagar = montoCo - descuento;
+            
+        } else if (aniosAn > 5) {
+            descuento = montoCo * 0.12;
+            totalPagar = montoCo - descuento;
+
+        }
+
+
+        model.addAttribute("resultado", "Felicidades, recibes un descuento de S/ " + descuento +
+                "<br>El total a pagar es: S/ " + totalPagar);
+
+        model.addAttribute("visualizaralerta", true);
+
+        return "descuentoCliente";
+    }
+
+
+
 }
--- src/main/java/pe/edu/idat/ec3_java_web/model/descuentoClienteModel.java
+++ src/main/java/pe/edu/idat/ec3_java_web/model/descuentoClienteModel.java
@@ -3,4 +3,22 @@ package pe.edu.idat.ec3_java_web.model;
 // AARON PINEDA
 
 public class descuentoClienteModel {
+    private Double monto;
+    private int anios;
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public int getAnios() {
+        return anios;
+    }
+
+    public void setAnios(int anios) {
+        this.anios = anios;
+    }
 }
